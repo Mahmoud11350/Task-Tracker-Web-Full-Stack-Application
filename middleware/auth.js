cfafb26a20e43287a28165b9023f11638b5d30ea
Mahmoud Salah <mahmoudsalah11350@gmail.com>
2022-01-09 23:30:47 +0200

    initial

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decode = jwt.verify(token, "Token");
+    const decode = jwt.verify(token, process.env.JWT_TOKEN);
     const user = await User.findOne({ _id: decode._id, "tokens.token": token });
     if (!user) {
       throw new Error();
